cd89219e462380b0f232475ee183edf68ff7239e
NguyenCong176708 <conghoahoa1cnh@gmail.com>
2021-03-05 11:00:58 +0700

    Updates

---
+++ precache-manifest.906534bede708c59582219ef28adfcc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a326d97704f98c38bc63bac08be007",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "b83a3cb27dbff48bf726",
+    "url": "/face-recognition-brain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "2e52867c70c1fb1ea3e8",
+    "url": "/face-recognition-brain/static/css/main.b6b15693.chunk.css"
+  },
+  {
+    "revision": "b83a3cb27dbff48bf726",
+    "url": "/face-recognition-brain/static/js/2.77121c4b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/face-recognition-brain/static/js/2.77121c4b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e52867c70c1fb1ea3e8",
+    "url": "/face-recognition-brain/static/js/main.9f939e44.chunk.js"
+  },
+  {
+    "revision": "cd099675855a957083bb",
+    "url": "/face-recognition-brain/static/js/runtime-main.b72ae444.js"
+  }
+]);
